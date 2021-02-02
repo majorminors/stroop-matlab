6b6208b60477d60b6e2678bc8d476a698d57b9f1
dorian <dorian@mino.rs>
2021-02-02 00:20:58 +0000

    fix typo in conversion script; remove png file of letters

--- lib/stimuli/img-scripts/svg-to-png-converter.sh
+++ lib/stimuli/img-scripts/svg-to-png-converter.sh
@@ -4,14 +4,14 @@
 
 # first convert from *.svg to *.svg.png
 echo "converting .svg files in current folder to .png files"
-wait 1s
+sleep 1s
 for f in *.svg; do
     inkscape --export-png="$f.png" --export-dpi=800 $f
 done
 
 # now remove that annoying `.svg` from the filename
 echo "now removing lingering .svg from filenames"
-wait 1s
+sleep 1s
 for filename in *.png; do 
     [ -f "$filename" ] || continue
     mv "$filename" "${filename//.svg/}"
