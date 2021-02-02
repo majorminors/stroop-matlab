38865ec0ff87abb4118147b794688ab43ce41817
dorian <dorian@mino.rs>
2021-02-02 00:16:31 +0000

    rename files to remove lingering .svg extensions; move script to subfolder

--- lib/stimuli/conv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for f in *.svg; do
-    inkscape --export-png="$f.png" --export-dpi=800 $f
-done
---
+++ lib/stimuli/img-scripts/svg-to-png-converter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# uses inkscape to convert all files with extension  `.svg` to `.png`
+
+# first convert from *.svg to *.svg.png
+echo "converting .svg files in current folder to .png files"
+wait 1s
+for f in *.svg; do
+    inkscape --export-png="$f.png" --export-dpi=800 $f
+done
+
+# now remove that annoying `.svg` from the filename
+echo "now removing lingering .svg from filenames"
+wait 1s
+for filename in *.png; do 
+    [ -f "$filename" ] || continue
+    mv "$filename" "${filename//.svg/}"
+done
+
+echo "done"
