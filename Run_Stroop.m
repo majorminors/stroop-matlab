000549069446dbd71f70d7b850cf22009d3afe18
dorian <dorian@mino.rs>
2021-02-14 20:19:58 +0000

    sort training from test stimuli

--- Run_Stroop.m
+++ Run_Stroop.m
@@ -11,7 +11,7 @@ clc;
 fprintf('setting up %s\n', mfilename);
 p = struct(); % est structure for parameter values
 d = struct(); % est structure for trial data
-t = struct(); % another structure for untidy trial specific floating variables that we might want to interrogate later if we mess up
+t = struct(); % another structure for untidy temp floating variables
 
 % initial settings
 rootdir = pwd; % root directory - used to inform directory mappings
@@ -19,10 +19,12 @@ p.vocal_stroop = 0;
 p.manual_stroop = 1;
 p.testing_enabled = 0; % change to 0 if not testing (1 skips PTB synctests) - see '% test variables' below
 p.fullscreen_enabled = 0;
-p.skip_synctests = 0; % skip ptb synctests
+p.skip_synctests = 1; % skip ptb synctests
 p.screen_num = 0;
+p.resp_keys = {'1!','2@','3#'}; % only accepts three response options
 p.colours = {'red','blue','green'}; % used to create response coding, will assume stimulus file is named with correct colours
 p.sizes = {'short','medium','tall'}; % used to create response coding
+p.quitkey = {'q'}; % keep this for vocal and manual
 p.screen_width = 40;   % Screen width in cm
 p.screen_height = 30;    % Screen height in cm
 p.screen_distance = 50; % Screen distance from participant in cm
@@ -30,7 +32,7 @@ p.screen_distance = 50; % Screen distance from participant in cm
 if p.vocal_stroop && p.manual_stroop; error('you have selected both vocal and manual stroop!'); end
 
 % directory mapping
-if ispc; setenv('PATH',[getenv('PATH') ';C:\Program Files\MATLAB\R2018a\toolbox\CBSU\Psychtoolbox\3.0.14\PsychContributed\x64']); end % make sure psychtoolbox has all it's stuff on pc
+%if ispc; setenv('PATH',[getenv('PATH') ';C:\Program Files\MATLAB\R2018a\toolbox\CBSU\Psychtoolbox\3.0.14\PsychContributed\x64']); end % make sure psychtoolbox has all it's stuff on pc
 addpath(genpath(fullfile(rootdir, 'lib'))); % add tools folder to path (includes moving_dots function which is required for dot motion, as well as an external copy of subfunctions for backwards compatibility with MATLAB)
 stimdir = fullfile(rootdir, 'lib', 'stimuli');
 datadir = fullfile(rootdir, 'data'); % will make a data directory if none exists
@@ -93,3 +95,42 @@ save(save_file); % save all data to a .mat file
 %% define experiment parameters
 
 fprintf('defining exp params for %s\n', mfilename);
+
+% define display info
+p.bg_colour = [255 255 255];
+p.text_colour = [0 0 0]; % colour of instructional text
+p.text_size = 40; % size of text
+p.window_size = [0 0 1200 800]; % size of window when ~p.fullscreen_enabled
+
+% timing info
+p.iti_time = 0.3; % inter trial inteval time
+p.trial_duration = 1.5; % seconds for the stimuli to be displayed
+p.feedback_time = 0.5; % period to display feedback after response
+p.min_stim_time = 0.2; % time to not show the stimulus
+
+%% define stimuli parameters
+
+fprintf('defining stimuli params for %s\n', mfilename);
+
+% read in stimuli files for the cue and put together a stimulus matrix
+t.stimuli = dir(fullfile(stimdir,'*.png')); % get the file info
+i = 0; % an index to work with
+stim = 0; % a stimulus counter
+tstim = 0; % a training stimulus counter
+while i < numel(t.stimuli) % loop through the files
+    i = i+1;
+    t.filename = t.stimuli(i).name; % get the filename
+    t.this_stim = t.filename(1:regexp(t.filename,'\.')-1); % get rid of the extension from the '.' on
+    if regexp(t.this_stim,'-') % if there's a hyphen (i.e. not a training stimulus and has two feature attributed in the filename)
+        stim = stim+1; % iterate stimulus counter
+        p.stimuli{stim,1} = t.this_stim; % add in the stimulus name
+        p.stimuli{stim,2} = imread(fullfile(stimdir, t.filename)); % read in the image
+    else
+        tstim = tstim+1; % iterate training stimulus counter
+        p.training_stimuli{tstim,1} = t.this_stim; % get the stimulus name
+        p.training_stimuli{tstim,2} = imread(fullfile(stimdir, t.filename)); % read in the image
+       
+    
+    end
+    
+end; clear i stim tstim
