56b8a0e59b895b569b69d32b8b067e2ae4ce3d00
dorian <dorian@mino.rs>
2021-04-11 22:48:39 +0100

    blocks added

--- lib/WordLocaliser/WordLocaliser.m
+++ lib/WordLocaliser/WordLocaliser.m
@@ -33,6 +33,7 @@ p.scanning = 1;
 p.buttonbox = 1;
 p.window_size = [0 0 1200 800]; % size of window when ~p.fullscreen_enabled
 
+p.numBlocks = 2;
 p.testTime = 16;
 p.stimTime = 0.5;
 p.itiTime = 0.1;
@@ -167,126 +168,132 @@ try
         KbWait(); % wait for a button press
     end
     
-    %%------------------------------------%%
-    %% shuffle test order and begin tests %%
-    %%------------------------------------%%
-    tmp = Shuffle(p.testInfo,[1]); % shuffle testInfo cols (keep rows)
-    t.tests = tmp(1,:); clear tmp % pull the tests into a temp variable
-    
-    for test = 1:length(t.tests)
-        t.trialType = t.tests{test};
+    for block = 1:p.numBlocks
         
-        % --- what are we doing for each test here? --- %
-        switch t.trialType
-            case p.testInfo{1,1}
-                t.stimuli = makeWordList(p.testInfo{2,1}); % make a word list from the file specified in the second row of the corresponding testinfo column
-            case p.testInfo{1,2}
-                t.stimuli = makeWordList(p.testInfo{2,2}); % make a word list from the file specified in the second row of the corresponding testinfo column
-            case p.testInfo{1,3}
-                t.stimuli = makeWordList(p.testInfo{2,2});
-                % make this false fonts and do something different here I
-                % think
-        end
+        %%------------------------------------%%
+        %% shuffle test order and begin tests %%
+        %%------------------------------------%%
+        tmp = Shuffle(p.testInfo,[1]); % shuffle testInfo cols (keep rows)
+        t.tests = tmp(1,:); clear tmp % pull the tests into a temp variable
         
-        % --- randomise the trial order and generate repeats --- %
-        t.randomisedOrder = zeros(1,p.numTrials); % init this with all same values
-        while length(unique(t.randomisedOrder)) ~= p.numTrials % let's make these all unique - repeats are hard to deal with in code and might be confusing at edge cases for participants
-            t.randomisedOrder = randi(length(t.stimuli),p.numTrials,1);
-        end
-        
-        % take a random sample of our order to repeat
-        t.toBeRepeated = zeros(1,p.numRepeats); % init this as all the same
-        while length(unique(t.toBeRepeated)) ~= p.numRepeats || any(t.toBeRepeated==0) % keep doing this until you have three unique repeats (none of which are zero)
+        for test = 1:length(t.tests)
+            t.trialType = t.tests{test};
+            
+            % --- what are we doing for each test here? --- %
+            switch t.trialType
+                case p.testInfo{1,1}
+                    t.stimuli = makeWordList(p.testInfo{2,1}); % make a word list from the file specified in the second row of the corresponding testinfo column
+                case p.testInfo{1,2}
+                    t.stimuli = makeWordList(p.testInfo{2,2}); % make a word list from the file specified in the second row of the corresponding testinfo column
+                case p.testInfo{1,3}
+                    t.stimuli = makeWordList(p.testInfo{2,2});
+                    % make this false fonts and do something different here I
+                    % think
+            end
+            
+            % --- randomise the trial order and generate repeats --- %
+            t.randomisedOrder = zeros(1,p.numTrials); % init this with all same values
+            while length(unique(t.randomisedOrder)) ~= p.numTrials % let's make these all unique - repeats are hard to deal with in code and might be confusing at edge cases for participants
+                t.randomisedOrder = randi(length(t.stimuli),p.numTrials,1);
+            end
+            
+            % take a random sample of our order to repeat
+            t.toBeRepeated = zeros(1,p.numRepeats); % init this as all the same
+            while length(unique(t.toBeRepeated)) ~= p.numRepeats || any(t.toBeRepeated==0) % keep doing this until you have three unique repeats (none of which are zero)
+                for i = 1:p.numRepeats
+                    t.numSegments = floor(length(t.randomisedOrder)/p.numRepeats); % let's divide it up roughly evenly, so we're spacing the repeats out kind of equally
+                    t.toBeRepeated(i) = Sample(t.randomisedOrder(t.numSegments*i-t.numSegments+1:t.numSegments*i-1)); % anywhere up to the 2nd last element of the segment (or else our next bit of logic might make the segment one longer)
+                end; clear i
+            end
+            % now make that element+1 be the repeat - do it in a seperate loop, so
+            % we don't accidentally do a repeat of something that was already
+            % repeated (which would exponentially increase that item)
             for i = 1:p.numRepeats
-                t.numSegments = floor(length(t.randomisedOrder)/p.numRepeats); % let's divide it up roughly evenly, so we're spacing the repeats out kind of equally
-                t.toBeRepeated(i) = Sample(t.randomisedOrder(t.numSegments*i-t.numSegments+1:t.numSegments*i-1)); % anywhere up to the 2nd last element of the segment (or else our next bit of logic might make the segment one longer)
+                repeatIndex = find(t.randomisedOrder == t.toBeRepeated(i));
+                t.randomisedOrder(repeatIndex+1) = t.toBeRepeated(i);
+                t.repeatTrials(i) = repeatIndex+1; % pull this so we can code them
             end; clear i
-        end
-        % now make that element+1 be the repeat - do it in a seperate loop, so
-        % we don't accidentally do a repeat of something that was already
-        % repeated (which would exponentially increase that item)
-        for i = 1:p.numRepeats
-            repeatIndex = find(t.randomisedOrder == t.toBeRepeated(i));
-            t.randomisedOrder(repeatIndex+1) = t.toBeRepeated(i);
-            t.repeatTrials(i) = repeatIndex+1; % pull this so we can code them
-        end; clear i
-        
-        for trial = 1:p.numTrials
             
-            % -- set up the key queue -- %
-            if ~p.buttonbox
-                t.queuekeys = [KbName(p.resp_keys{1}), KbName(p.resp_keys{2}), KbName(p.resp_keys{3}), KbName(p.quitkey)]; % define the keys the queue cares about
-            else
-                t.queuekeys = [KbName(p.quitkey)]; % define the keys the queue cares about
-            end
-            t.queuekeylist = zeros(1,256); % create a list of all possible keys (all 'turned off' i.e. zeroes)
-            t.queuekeylist(t.queuekeys) = 1; % 'turn on' the keys we care about in the list (make them ones)
-            KbQueueCreate([], t.queuekeylist); % initialises queue to collect response information from the list we made (not listening for response yet)
-            KbQueueStart(); % starts delivering keypress info to the queue
-            
-            % --- pull and display the stimulus for the trial --- %
-            %     showImage(p,imageLocation);
-            t.thisStim = t.stimuli{t.randomisedOrder(trial)}; % pull it out of the cell
-            showText(p,t.thisStim);
-            
-            % wait for stimulus time
-            WaitSecs(p.stimTime);
-            
-            %% --- iti+saving --- %%
-            
-            Screen('Flip', p.window);
-              
-            WaitSecs(p.itiTime);
-            
-            % coding
-            d.results{1,trial,test} = t.trialType; % code trial type
-            if any(t.repeatTrials == trial) % if this is a repeat trial
-                d.results{2,trial,test} = 1;
-            else
-                d.results{2,trial,test} = 0;
-            end
-            
-            % deal with keypress (required for keyboard incl quitkey)
-            [t.pressed,t.firstPress] = KbQueueCheck(); % check for keypress in the KbQueue
-            if t.pressed
-                t.resp_key_name = KbName(t.firstPress); % get the name of the key used to respond - might need squiggly brackets?
-                if size(t.resp_key_name) > 1; t.resp_key_name = t.resp_key_name{1}; end % just get the first entry (if two are pressed together)
-            else; t.resp_key_name = NaN; end
-            t.resp_key_time = sum(t.firstPress); % get the timing info of the key used to respond
-            
-            % quit if quitkey
-            if strcmp(t.resp_key_name,p.quitkey)
-                save(save_file); % so we don't lose all data
-                fclose('all');
-                error('%s quit by user (p.quitkey pressed)\n', mfilename);
-            end
-            
-            % code whether they responded or not
-            if p.buttonbox
-            else
-                if t.pressed % since I don't think we care what they pressed (and it will have already quit if they wanted to quit)
-                    d.results{3,trial,test} = 1;
+            for trial = 1:p.numTrials
+                
+                % -- set up the key queue -- %
+                if ~p.buttonbox
+                    t.queuekeys = [KbName(p.resp_keys{1}), KbName(p.resp_keys{2}), KbName(p.resp_keys{3}), KbName(p.quitkey)]; % define the keys the queue cares about
                 else
-                    d.results{3,trial,test} = 0;
+                    t.queuekeys = [KbName(p.quitkey)]; % define the keys the queue cares about
                 end
-            end
-            % code correct or incorrect
-            if d.results{2,trial,test} == d.results{3,trial,test} % if they responded when they should have, or didn't when they shouldn't
-                d.results{4,trial,test} = 1; % correct
-            else
-                d.results{4,trial,test} = 0; % incorrect
-            end
+                t.queuekeylist = zeros(1,256); % create a list of all possible keys (all 'turned off' i.e. zeroes)
+                t.queuekeylist(t.queuekeys) = 1; % 'turn on' the keys we care about in the list (make them ones)
+                KbQueueCreate([], t.queuekeylist); % initialises queue to collect response information from the list we made (not listening for response yet)
+                KbQueueStart(); % starts delivering keypress info to the queue
+                
+                % --- pull and display the stimulus for the trial --- %
+                %     showImage(p,imageLocation);
+                t.thisStim = t.stimuli{t.randomisedOrder(trial)}; % pull it out of the cell
+                showText(p,t.thisStim);
+                
+                % wait for stimulus time
+                WaitSecs(p.stimTime);
+                
+                %% --- iti+saving --- %%
+                
+                Screen('Flip', p.window);
+                
+                WaitSecs(p.itiTime);
+                
+                % coding
+                d.results{1,trial,test,block} = t.trialType; % code trial type
+                if any(t.repeatTrials == trial) % if this is a repeat trial
+                    d.results{2,trial,test,block} = 1;
+                else
+                    d.results{2,trial,test,block} = 0;
+                end
+                
+                % deal with keypress (required for keyboard incl quitkey)
+                [t.pressed,t.firstPress] = KbQueueCheck(); % check for keypress in the KbQueue
+                if t.pressed
+                    t.resp_key_name = KbName(t.firstPress); % get the name of the key used to respond - might need squiggly brackets?
+                    if size(t.resp_key_name) > 1; t.resp_key_name = t.resp_key_name{1}; end % just get the first entry (if two are pressed together)
+                else; t.resp_key_name = NaN; end
+                t.resp_key_time = sum(t.firstPress); % get the timing info of the key used to respond
+                
+                % quit if quitkey
+                if strcmp(t.resp_key_name,p.quitkey)
+                    save(save_file); % so we don't lose all data
+                    fclose('all');
+                    error('%s quit by user (p.quitkey pressed)\n', mfilename);
+                end
+                
+                % code whether they responded or not
+                if p.buttonbox
+                else
+                    if t.pressed % since I don't think we care what they pressed (and it will have already quit if they wanted to quit)
+                        d.results{3,trial,test,block} = 1;
+                    else
+                        d.results{3,trial,test,block} = 0;
+                    end
+                end
+                % code correct or incorrect
+                if d.results{2,trial,test,block} == d.results{3,trial,test,block} % if they responded when they should have, or didn't when they shouldn't
+                    d.results{4,trial,test,block} = 1; % correct
+                else
+                    d.results{4,trial,test,block} = 0; % incorrect
+                end
+                
+                save(save_file); % save all data to a .mat file
+                
+                %% --- post trial cleanup --- %%
+                KbQueueRelease();
+                
+            end; clear trial
             
             save(save_file); % save all data to a .mat file
             
-            %% --- post trial cleanup --- %%
-            KbQueueRelease();
-            
-        end; clear trial
+        end; clear test
         
         save(save_file); % save all data to a .mat file
         
-    end; clear test
+    end; clear block
     
     % --- wrap up --- %
     save(save_file); % save all data to a .mat file
