9b26539988345c792ac0c2ae58d12bb13c7f8c8e
dorian <dorian@mino.rs>
2021-04-18 12:48:24 +0100

    add word and pseudoword stimuli

--- lib/WordLocaliser/stimuli/line-to-png.sh
+++ lib/WordLocaliser/stimuli/line-to-png.sh
@@ -5,5 +5,6 @@ count=0
 mkdir "$2"
 for line in $lines; do
     (( count++ ))
-    convert -size 350x100 xc:none -fill white -stroke white -font BACS2sans -gravity Center -pointsize 100 -annotate +0+0 "$line" $savedir/$count.png
+    # convert -size 350x100 xc:none -fill white -stroke white -font BACS2serif -gravity Center -pointsize 100 -annotate +0+0 "$line" $savedir/$count.png
+    convert -size 350x100 xc:none -fill white -stroke white -font Courier -gravity Center -pointsize 100 -annotate +0+0 "$line" $savedir/$count.png
 done
