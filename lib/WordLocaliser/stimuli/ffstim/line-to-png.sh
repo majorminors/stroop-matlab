6687b2c4754e03432ef69de4db94404dc3bf5ace
dorian <dorian@mino.rs>
2021-04-18 11:49:27 +0100

    update falsefonts and wordlist

--- lib/WordLocaliser/stimuli/ffstim/line-to-png.sh
+++ lib/WordLocaliser/stimuli/ffstim/line-to-png.sh
@@ -2,6 +2,7 @@
 lines=$(cat $1)
 savedir=$2
 count=0
+mkdir "$2"
 for line in $lines; do
     (( count++ ))
     convert -size 350x100 xc:none -fill white -stroke white -font BACS2sans -gravity Center -pointsize 100 -annotate +0+0 "$line" $savedir/$count.png
